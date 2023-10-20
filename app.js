d5169830e67f2f79b8af3fb3e401006a8a5f20dc
as.kovinko <as.kovinko@tensor.ru>
2023-10-20 17:03:49 +0300

    files

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+tg.MainButton.show();
+
+
 let result = "";
 
 let x_gypsum = document.getElementById("x_gypsum");
@@ -12,6 +18,9 @@ let x_all = document.getElementById("x_all");
 let calc = document.getElementById("calc");
 
 let input_gypsum = document.getElementById('gypsum')
+let input_water = document.getElementById('water')
+let input_plasticizer = document.getElementById('plasticizer')
+let input_pigment = document.getElementById('pigment')
 
 function clear_value(value) {
 	let item = document.getElementById(value);
@@ -22,20 +31,16 @@ input_gypsum.addEventListener("click", function (){
 	clear_value('gypsum')
 });
 
-x_gypsum.addEventListener("click", function(){
+input_gypsum.addEventListener("click", function (){
 	clear_value('gypsum')
 });
 
-x_water.addEventListener("click", function(){
-	clear_value('water')
+input_gypsum.addEventListener("click", function (){
+	clear_value('gypsum')
 });
 
-x_plasticizer.addEventListener("click", function(){
-	clear_value('plasticizer')
-});
-
-x_pigment.addEventListener("click", function(){
-	clear_value('pigment')
+input_gypsum.addEventListener("click", function (){
+	clear_value('gypsum')
 });
 
 x_all.addEventListener("click", function(){
@@ -45,7 +50,33 @@ x_all.addEventListener("click", function(){
 	}
 });
 
-calc.addEventListener("click", function(){
+
+// calc.addEventListener("click", function(){
+// 	let gypsum = 1000;
+// 	let values_water = document.getElementById('water').value;
+// 	let values_plasticizer = document.getElementById('plasticizer').value;
+// 	let values_pigment = document.getElementById('pigment').value;
+// 	let values_form = document.getElementById('value').value;
+// 	let name = document.getElementById('name').value;
+// 	let ratio_gypsum = document.getElementById("gypsum").value;
+// 	let Result = document.getElementById('Result')
+//
+// 	let gypsum_value = values_form * ratio_gypsum;
+// 	let need_water = (values_water / gypsum) * gypsum_value;
+// 	let need_plasticizer = (values_plasticizer / gypsum) * gypsum_value;
+// 	let need_pigment = (values_pigment / gypsum) * gypsum_value;
+//
+// 	result = name + '\n' +
+// 		    'гипс: ' + Number(gypsum_value).toFixed() + 'гр (' + gypsum_value.toFixed(2) + ')\n' +
+// 			'воды: ' + Number(need_water).toFixed() + 'гр (' + need_water.toFixed(2) + ')\n' +
+// 			'пластификатор: ' + Number(need_plasticizer).toFixed() + 'гр (' + need_plasticizer.toFixed(2) + ')\n' +
+// 			'пигмент: ' + Number(need_pigment).toFixed() + 'гр (' + need_pigment.toFixed(2) + ')';
+// 	Result.innerText = result
+// 	// tg.sendData(result)
+// });
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	let gypsum = 1000;
 	let values_water = document.getElementById('water').value;
 	let values_plasticizer = document.getElementById('plasticizer').value;
@@ -66,13 +97,7 @@ calc.addEventListener("click", function(){
 			'пластификатор: ' + Number(need_plasticizer).toFixed() + 'гр (' + need_plasticizer.toFixed(2) + ')\n' +
 			'пигмент: ' + Number(need_pigment).toFixed() + 'гр (' + need_pigment.toFixed(2) + ')';
 	Result.innerText = result
-	// tg.sendData(result)
 });
-
-
-// Telegram.WebApp.onEvent("mainButtonClicked", function(){
-// 	tg.sendData(item);
-// });
 
 // function addActive(x) {
 //     if (!x) return false;
